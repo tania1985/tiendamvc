ac709139e72975196e15bc97f6637bc3fba1c9aa
Tonitf <subcero00@hotmail.com>
2025-02-25 17:08:06 +0100

    provider

---
+++ app/models/Provider.php
@@ -0,0 +1,10 @@
+<?php
+namespace Formacom\Models;
+use Illuminate\Database\Eloquent\Model;
+class User extends Model{
+    protected $table="provider";
+    protected $primaryKey = 'provider_id';
+
+}
+
+?>
