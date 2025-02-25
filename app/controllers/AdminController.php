1916bae684ed7209e9213c0e446efaa798cb130c
Tonitf <subcero00@hotmail.com>
2025-02-25 15:52:04 +0100

    limpiar programa

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -1,13 +1,11 @@
 <?php
 namespace Formacom\controllers;
 use Formacom\Core\Controller;
-class HomeController extends Controller{
+class AdminController extends Controller{
     public function index(...$params){
         $data = ['mensaje' => '¡Bienvenido a la página de inicio!'];
         $this->view('home', $data);
     }
-    public function new(){
-        echo "Hola desde New de HomeController";
-    }
+   
 }
 ?>
