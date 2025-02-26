38dc279a729cf238a47f3155b7360b18b3bee417
tonitf <subcero00@hotmail.com>
2025-02-26 12:55:30 +0100

    Arreglando array en controller

--- core/Controller.php
+++ core/Controller.php
@@ -12,6 +12,12 @@ abstract class Controller
         $className = end($parts);
         // Convertimos a minúsculas y removemos la palabra "Controller" para obtener el nombre de la carpeta
         $controllerName = strtolower(str_replace("Controller", "", $className));
+        
+        // Si $data no es un array, lo envolvemos
+        if (!is_array($data)) {
+            $data = ['data' => $data];
+        }
+        
         extract($data);
         require_once './app/views/'.$controllerName.'/'. $view . '.php';
     }
